12a2f0ec5d948c4058bc9e9c1485cb14deecd56f
Shehanka <hcsperera@outlook.com>
2021-01-11 23:36:18 +0530

    Single product done

---
+++ src/pages/ProductPage/ProductPage.tsx
@@ -0,0 +1,13 @@
+import ProductBanner from "../../modules/ProductModule/ProductBanner";
+import ProductView from "../../modules/ProductModule/ProductView";
+
+function ProductPage() {
+  return (
+    <>
+      <ProductBanner />
+      <ProductView />
+    </>
+  );
+}
+
+export default ProductPage;
--- src/routes/appRoute.ts
+++ src/routes/appRoute.ts
@@ -1,7 +1,9 @@
 import HomePage from "../pages/HomePage/HomePage";
+import ProductPage from "../pages/ProductPage/ProductPage";
 import ShopPage from "../pages/ShopPage/ShopPage";
 
 export const routes = [
   { path: "/", component: HomePage },
   { path: "/shop", component: ShopPage },
+  { path: "/product", component: ProductPage },
 ];
