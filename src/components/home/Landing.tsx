dfb6e211f75eef81588aa84b2050ce4226edd51e
ebonynon <sithumsandeepap@gmail.com>
2021-01-11 18:34:00 +0530

    Fix akka's mouse issue

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -9,13 +9,12 @@ function Landing(): JSX.Element {
         className="hero-wrap js-fullheight"
         style={{ backgroundImage: "url(" + IMG + ")", height: "100vh" }}
       >
-        <div className="overlay" />
+        <div className="overlay"></div>
         <div className="container">
-          <div className="row no-gutters slider-text align-items-center justify-content-center">
+          <div className="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
             <h3 className="v">Modist - Time to get dress</h3>
             <h3 className="vr">Since - 1985</h3>
-            {/* <div className="col-md-11 ftco-animate text-center fadeInUp ftco-animated"> */}
-            <div className="text-center" style={{ marginTop: "14.5rem" }}>
+            <div className="col-md-11 ftco-animate text-center">
               <h1>Le Stylist</h1>
               <h2>
                 <span>Wear Your Dress</span>
@@ -24,7 +23,7 @@ function Landing(): JSX.Element {
             <div className="mouse">
               <a href="#" className="mouse-icon">
                 <div className="mouse-wheel">
-                  <span className="ion-ios-arrow-down" />
+                  <span className="ion-ios-arrow-down"></span>
                 </div>
               </a>
             </div>
