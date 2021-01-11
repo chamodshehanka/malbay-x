899f66cd956f94a50d178cd68f81f224f2113a01
Shehanka <hcsperera@outlook.com>
2021-01-12 00:09:59 +0530

    Checkout Form done

---
+++ src/modules/CheckoutPage/CheckoutBanner.tsx
@@ -0,0 +1,28 @@
+import BG6 from "../../assets/images/bg_6.jpg";
+
+function CheckoutBanner() {
+  return (
+    <>
+      <div
+        className="hero-wrap hero-bread"
+        style={{ backgroundImage: `url(${BG6})` }}
+      >
+        <div className="container">
+          <div className="row no-gutters slider-text align-items-center justify-content-center">
+            <div className="col-md-9 ftco-animate text-center">
+              <h1 className="mb-0 bread">Checkout</h1>
+              <p className="breadcrumbs">
+                <span className="mr-2">
+                  <a href="/">Home</a>
+                </span>
+                <span>Cart</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CheckoutBanner;
---
+++ src/pages/CheckoutPage/CheckoutPage.tsx
@@ -0,0 +1,13 @@
+import CheckoutBanner from "../../modules/CheckoutPage/CheckoutBanner";
+import CheckoutForm from "../../modules/CheckoutPage/CheckoutForm";
+
+function CheckoutPage() {
+  return (
+    <>
+      <CheckoutBanner />
+      <CheckoutForm />
+    </>
+  );
+}
+
+export default CheckoutPage;
