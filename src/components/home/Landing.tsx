805523ff0f16fdd102fe1d945cc34a7a59857308
Shehanka <hcsperera@outlook.com>
2021-01-11 14:15:05 +0530

    Landing making

---
+++ src/components/home/Landing.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Landing(): JSX.Element {
+  return (
+    <>
+      <div
+        className="hero-wrap js-fullheight"
+        style={{ backgroundImage: require("../../assets/images/bg_1.jpg") }}
+      >
+        <div className="overlay" />
+        <div className="container">
+          <div className="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+            <h3 className="v">Modist - Time to get dress</h3>
+            <h3 className="vr">Since - 1985</h3>
+            <div className="col-md-11 ftco-animate text-center">
+              <h1>Le Stylist</h1>
+              <h2>
+                <span>Wear Your Dress</span>
+              </h2>
+            </div>
+            <div className="mouse">
+              <a href="#" className="mouse-icon">
+                <div className="mouse-wheel">
+                  <span className="ion-ios-arrow-down" />
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Landing;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../../components/common/Navbar/Navbar";
+import Landing from "../../components/home/Landing";
 
 function HomePage(): JSX.Element {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
 }
 
 export default HomePage;
