f402eb3fed608760a9f1cb39410b5da08e9184fc
Shehanka <hcsperera@outlook.com>
2021-01-11 13:24:50 +0530

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { ReactQueryDevtools } from "react-query-devtools";
+import HomePage from "./pages/HomePage/HomePage";
 import { routes } from "./routes/appRoute";
 import { SnackbarProvider } from "notistack";
 import { DeletePromptProvider } from "./components/common/DeletePrompt/DeletePrompt";
@@ -12,7 +13,8 @@ function App(): JSX.Element {
       <ReactQueryDevtools initialIsOpen={false} />
       <DeletePromptProvider>
         <SnackbarProvider>
-          <Switch>
+          <HomePage />
+          <Router>
             {routes.map((route) => (
               <Route
                 key={route.path}
@@ -21,7 +23,7 @@ function App(): JSX.Element {
                 component={route.component}
               />
             ))}
-          </Switch>
+          </Router>
         </SnackbarProvider>
       </DeletePromptProvider>
     </div>
