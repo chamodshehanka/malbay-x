9be0af1f035f40f0a50640ef882b2bab3f4ae6b3
Shehanka <hcsperera@outlook.com>
2021-01-13 03:19:42 +0530

    Product Loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { ReactQueryDevtools } from "react-query-devtools";
@@ -6,24 +7,28 @@ import { routes } from "./routes/appRoute";
 import { SnackbarProvider } from "notistack";
 import { DeletePromptProvider } from "./components/common/DeletePrompt/DeletePrompt";
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
     <div>
       <ReactQueryDevtools initialIsOpen={false} />
-      <DeletePromptProvider>
-        <SnackbarProvider>
-          <Router>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                exact
-                path={route.path}
-                component={route.component}
-              />
-            ))}
-          </Router>
-        </SnackbarProvider>
-      </DeletePromptProvider>
+      <QueryClientProvider client={queryClient}>
+        <DeletePromptProvider>
+          <SnackbarProvider>
+            <Router>
+              {routes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact
+                  path={route.path}
+                  component={route.component}
+                />
+              ))}
+            </Router>
+          </SnackbarProvider>
+        </DeletePromptProvider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/modules/ShopModule/DressCardSection.tsx
+++ src/modules/ShopModule/DressCardSection.tsx
@@ -1,11 +1,11 @@
 import product1 from "../../assets/images/product-1.jpg";
-import product2 from "../../assets/images/product-2.jpg";
-import product3 from "../../assets/images/product-3.jpg";
-import product4 from "../../assets/images/product-4.jpg";
-import product5 from "../../assets/images/product-5.jpg";
-import product6 from "../../assets/images/product-6.jpg";
-import product7 from "../../assets/images/product-7.jpg";
-import product8 from "../../assets/images/product-8.jpg";
+// import product2 from "../../assets/images/product-2.jpg";
+// import product3 from "../../assets/images/product-3.jpg";
+// import product4 from "../../assets/images/product-4.jpg";
+// import product5 from "../../assets/images/product-5.jpg";
+// import product6 from "../../assets/images/product-6.jpg";
+// import product7 from "../../assets/images/product-7.jpg";
+// import product8 from "../../assets/images/product-8.jpg";
 import ProductCard from "../../components/ShopPage/ProductCard";
 import { useGetProducts } from "../../queries/useGetProducts";
 
@@ -13,7 +13,11 @@ import { useGetProducts } from "../../queries/useGetProducts";
 // page has 2 rows
 
 export default function DressCardSection() {
-  const { data: productsList = [], status: productStatus } = useGetProducts();
+  const {
+    data: productsList = [],
+    status: productStatus,
+    isSuccess,
+  } = useGetProducts();
 
   // TODO: List from here
   console.log(productsList, productStatus);
@@ -23,62 +27,73 @@ export default function DressCardSection() {
       <section className="ftco-section bg-light">
         <div className="container-fluid">
           <div className="row">
-            <ProductCard
-              img={product1}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product2}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product3}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product4}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product5}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product6}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product7}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
-            <ProductCard
-              img={product8}
-              status={"NEW 30%"}
-              title={"Young Woman Wearing Dress"}
-              link={"#"}
-              price={200}
-            />
+            {isSuccess &&
+              productsList.map((p) => (
+                <ProductCard
+                  key={p?._id}
+                  img={product1}
+                  status={"NEW 30%"}
+                  title={p.name}
+                  link={"#"}
+                  price={200}
+                />
+              ))}
+            {/*<ProductCard*/}
+            {/*  img={product1}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product2}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product3}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product4}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product5}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product6}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product7}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
+            {/*<ProductCard*/}
+            {/*  img={product8}*/}
+            {/*  status={"NEW 30%"}*/}
+            {/*  title={"Young Woman Wearing Dress"}*/}
+            {/*  link={"#"}*/}
+            {/*  price={200}*/}
+            {/*/>*/}
           </div>
         </div>
       </section>
