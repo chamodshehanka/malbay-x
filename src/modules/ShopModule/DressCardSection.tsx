d2e2392849e9c14819b7d19a2d5d775d8382b664
Shehanka <hcsperera@outlook.com>
2021-01-25 04:12:33 +0530

    Product view page making

--- src/modules/ShopModule/DressCardSection.tsx
+++ src/modules/ShopModule/DressCardSection.tsx
@@ -2,15 +2,21 @@ import ProductCard from "../../components/ShopPage/ProductCard";
 import { useGetProducts } from "../../queries/useGetProducts";
 
 export default function DressCardSection() {
-  const { data: productsList = [], isSuccess } = useGetProducts();
+  const {
+    data: productsList = [],
+    isSuccess,
+    status: productListStatus,
+  } = useGetProducts();
 
+  const hasProductData = productListStatus === "success";
+  console.log(productsList);
   return (
     <>
       <section className="ftco-section bg-light">
         <div className="container-fluid">
           <div className="row">
-            {isSuccess &&
-              productsList.map((p) => (
+            {hasProductData &&
+              productsList?.map((p) => (
                 <ProductCard
                   key={p?.ID}
                   img={p?.Image}
--- src/queries/useGetProducts.ts
+++ src/queries/useGetProducts.ts
@@ -6,3 +6,9 @@ const PRODUCT_LIST = "PRODUCT_LIST";
 export function useGetProducts() {
   return useQuery(PRODUCT_LIST, Product_API.getAllProducts);
 }
+
+const PRODUCT_ITEM = "PRODUCT_ITEM";
+
+export function useGetProductById(productId: string) {
+  return useQuery([PRODUCT_ITEM, productId], () => Product_API.getProductById);
+}
